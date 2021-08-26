cc53754d9d0ba6e80a7f154bef0bcd08dd1d075d
Wei-Cheng Pan <legnaleurc@gmail.com>
2021-08-27 04:43:24 +0900

    refactor

--- pcrd/ocr.py
+++ pcrd/ocr.py
@@ -7,16 +7,19 @@ import cv2
 import numpy
 
 
+DigitList = list[tuple[int, numpy.ndarray]]
+
+
 METHOD = cv2.TM_CCOEFF_NORMED
 ITEM_SIZE = (160, 160)
 
 
-def load_input(input_path: str):
+def load_input(input_path: str) -> list[numpy.ndarray]:
     path_list = os.listdir(input_path)
     return [cv2.imread(f'{input_path}/{path}') for path in path_list]
 
 
-def load_digits():
+def load_digits() -> DigitList:
     path_list = os.listdir('./digits')
     return [
         (get_digit_from_path(path), load_digit('./digits/' + path))
@@ -24,7 +27,7 @@ def load_digits():
     ]
 
 
-def load_items():
+def load_items() -> list[tuple[str, numpy.ndarray]]:
     path_list = os.listdir('./items')
     return [
         (get_item_id_from_path(path), load_item('./items/' + path))
@@ -56,32 +59,35 @@ def load_digit(path: str) -> numpy.ndarray:
     return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
 
-def get_item_count(source, item, digit_list, id):
-    item = cv2.resize(item, (160, 160), cv2.INTER_AREA)
+def get_item_count(source: numpy.ndarray, item: numpy.ndarray, digit_list: DigitList, id: str) -> int:
+    # match item
     rv = cv2.matchTemplate(source, item, METHOD)
-
     _min_value, max_value, _min_loc, max_loc = cv2.minMaxLoc(rv, None)
-
     if max_value < 0.8:
         return 0
-
     match_loc = max_loc
 
+    # get number part in the source
     min_y = match_loc[1] + 125
     max_y = match_loc[1] + item.shape[1] - 12
     min_x = match_loc[0] + 79
     max_x = match_loc[0] + item.shape[0] - 5
-    display = source[min_y:max_y, min_x:max_x]
+    display: numpy.ndarray = source[min_y:max_y, min_x:max_x]
 
+    # filter out black
     display = cv2.cvtColor(display, cv2.COLOR_BGR2HSV)
     lower = numpy.array([0, 0, 60], dtype=numpy.uint8)
     upper = numpy.array([255, 255, 145], dtype=numpy.uint8)
-    mask = cv2.inRange(display, lower, upper)
+    mask: numpy.ndarray = cv2.inRange(display, lower, upper)
+    # invert color
     mask = ~mask
-    _y, x, _ = display.shape
-    grid_list = [mask[:, x - 19 * (i + 1):x - 19 * i] for i in range(4)]
 
-    total = 0
+    # split digits
+    _y, x, _ = display.shape
+    grid_list: list[numpy.ndarray] = [mask[:, x - 19 * (i + 1):x - 19 * i] for i in range(4)]
+
+    # convert to digits
+    total: int = 0
     for idx, grid in enumerate(grid_list):
         rv = get_digit(grid, digit_list, id, idx)
         if rv < 0:
@@ -94,7 +100,7 @@ def get_item_count(source, item, digit_list, id):
     return total
 
 
-def get_digit(grid, digit_list, id, idx):
+def get_digit(grid: numpy.ndarray, digit_list: DigitList, id: str, idx: int) -> int:
     best_score = 0
     best_number = -1
     for number, digit in digit_list:
