d693b4d924a7b59f8340d5a55b4f00abafb61b74
Wei-Cheng Pan <legnaleurc@gmail.com>
2021-08-31 02:21:51 +0900

    add argument

---
+++ pcrd/adb.py
@@ -0,0 +1,55 @@
+import subprocess
+import time
+
+import cv2
+import numpy
+
+
+def grab_screenshot(adb_path: str) -> numpy.ndarray:
+    with subprocess.Popen(f'{adb_path} shell screencap -p', stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True) as pipe:
+        image_bytes = pipe.stdout.read().replace(b'\r\n', b'\n')
+    np_arr = numpy.frombuffer(image_bytes, numpy.uint8)
+    return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+
+def swipe(adb_path: str, x1: int, y1: int, x2: int, y2: int, duration: int) -> None:
+    subprocess.call(f'{adb_path} shell input swipe {x1} {y1} {x2} {y2} {duration}', shell=True)
+
+
+def get_coordinates(h: int, w: int) -> tuple[int, int, int, int]:
+    return w // 2, 3 * h // 4, w // 2, h // 3.5
+
+
+def get_bottom_point(h: int, w: int) -> tuple[int, int]:
+    return 749 * h // 900, 977 * w // 1600
+
+
+def get_color_diff(a: tuple, b: tuple) -> int:
+    ret = 0
+    power = [2, 4, 3]
+    for i in range(3):
+        ret += power[i] * (a[i] - b[i]) ** 2
+    return ret
+
+
+def is_scroll_to_botoom(scroll_bar_color: tuple[int, int, int]) -> bool:
+    SCROLL_BAR_COLOR = (191, 123, 88)
+    return get_color_diff(scroll_bar_color, SCROLL_BAR_COLOR) < 200
+
+
+def grab_input(input_folder: str, adb_path: str) -> None:
+    coordinates = None
+    scroll_bar_bottom_point = None
+    idx = 0
+    while True:
+        img = grab_screenshot(adb_path)
+        if not coordinates:
+            h, w, _ = img.shape
+            coordinates = get_coordinates(h, w)
+            scroll_bar_bottom_point = get_bottom_point(h, w)
+        cv2.imwrite(f'{input_folder}/{idx}.png', img)
+        if is_scroll_to_botoom(img[scroll_bar_bottom_point]):
+            break
+        swipe(adb_path, *coordinates, 500)
+        time.sleep(0.5)
+        idx += 1
--- pcrd/main.py
+++ pcrd/main.py
@@ -1,8 +1,10 @@
 import argparse
 import concurrent.futures
+import os
+import os.path
 import sys
 
-from .input import grab_input
+from .adb import grab_input
 from .ocr import load_digits, load_items, load_input, get_item_count
 from .output import generate_script
 
@@ -11,8 +13,8 @@ def main(args: list[str] = None) -> int:
     if args is None:
         args = sys.argv
     kwargs = parse_args(args[1:])
-    if not kwargs.is_input_exist:
-        grab_input(kwargs.input_folder)
+    if kwargs.auto_screen_shot:
+        grab_input(kwargs.input_folder, kwargs.adb_path)
 
     input_list = load_input(kwargs.input_folder)
     digit_list = load_digits()
@@ -42,12 +44,46 @@ def main(args: list[str] = None) -> int:
 def parse_args(args: list[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser('pcrd')
 
-    parser.add_argument('input_folder', type=str, default='./input')
-    parser.add_argument('output_file', type=str, default='./output.js')
-    parser.add_argument('is_input_exist', type=int, default=0)
+    parser.add_argument('input_folder', type=str, help='folder contains item screen shots (e.g. ./input/)')
+    parser.add_argument('output_file', type=str, help='file name of JS output (e.g. ./output.js)')
+    add_bool_argument(parser, 'auto_screen_shot', 'A',
+        default=True,
+        yes_help='enable auto screen shot (need adb)',
+        no_help='disable auto screen shot',
+    )
+    parser.add_argument('--adb-path', type=str, action='store', default='adb')
 
     kwargs = parser.parse_args(args)
+    check_args(kwargs)
     return kwargs
+
+
+def check_args(kwargs: argparse.Namespace):
+    if not os.path.isdir(kwargs.input_folder):
+        raise argparse.ArgumentError(None, f'input folder {kwargs.input_folder} not found')
+    if kwargs.auto_screen_shot:
+        if os.listdir(kwargs.input_folder):
+            raise argparse.ArgumentError(None, f'input folder {kwargs.input_folder} is not empty')
+
+
+def add_bool_argument(
+    parser: argparse.ArgumentParser,
+    name: str,
+    short_name: str = None,
+    *,
+    default: bool = True,
+    yes_help: str = None,
+    no_help: str = None,
+) -> None:
+    flag = name.replace('_', '-')
+    pos_flags = ['--' + flag]
+    if short_name:
+        pos_flags.append('-' + short_name)
+    neg_flag = '--no-' + flag
+    pos_default = default
+    neg_default = not default
+    parser.add_argument(*pos_flags, dest=name, action='store_true', default=pos_default, help=yes_help)
+    parser.add_argument(neg_flag, dest=name, action='store_false', default=neg_default, help=no_help)
 
 
 def find_item_count(input_list, digit_list, id, item) -> tuple[str, int]:
