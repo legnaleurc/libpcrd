8d3d6cac86ad118a3a2851d973ee6f847a3657d8
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-08-16 22:47:32 +0900

    update import script

--- tools/curl_items.js
+++ tools/curl_items.js
@@ -1,15 +1,15 @@
 (() => {
   const shell = document.querySelectorAll('.item-title')[2].parentElement;
-  const itemList = document.querySelectorAll('.itemBox > img');
-  const urlList = Array.prototype.map.call(itemList, (item) => item.src);
-  const cmdList = urlList.map((url) => `curl -O ${url}`);
+  const itemList = document.querySelectorAll('.item-box .itemBox > img');
+  const urlList = Array.prototype.map.call(itemList, (item) => new URL(item.dataset.src, location.origin));
+  const cmdList = urlList.map((url) => `curl -O ${url.toString()}`);
 
   const box = document.createElement('div');
   box.style.position = 'absolute';
   box.style.top = 0;
   box.style.left = 0;
   const ta = document.createElement('textarea');
-  ta.rows = 100;
+  ta.rows = 80;
   ta.value = cmdList.join('\n') + '\n';
   box.appendChild(ta);
   shell.appendChild(box);
